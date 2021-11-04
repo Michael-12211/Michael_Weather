f195c12da01c3d0c8e720602480be0262fae3271
Michael-12211 <michaeldavidkempe@gmail.com>
2021-11-04 17:27:48 -0400

    Initial Commit

---
+++ Michael_Weather/Michael_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Michael_WeatherApp.swift
+//  Michael_Weather
+//
+//  Created by Michael Kempe on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct Michael_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
